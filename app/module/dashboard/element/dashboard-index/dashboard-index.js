62ceea449ce0f839533a224260091d84d49e2842
Paolo <paolo.sartorio@outlook.it>
2020-11-03 11:55:41 +0100

    Added icons and completed some sections

--- app/module/dashboard/element/dashboard-index/dashboard-index.js
+++ app/module/dashboard/element/dashboard-index/dashboard-index.js
@@ -115,10 +115,21 @@ class DashboardIndex extends ServiceInjectorMixin(PolymerElement) {
   text-transform: uppercase;
   text-decoration: none;
   background: #015b63;
-  border-radius: 15px;
-  padding: 9px 30px;
+  border-radius: 10px;
+  padding: 10px 40px;
   display: inline-block;
   margin-top: 30px;
+}
+
+.button:hover {
+  transform:scale(1.1);
+  transition: all ease 0.2s;
+}
+
+.button.yellow {
+  color: black;
+  background: #f0b906;
+  transition: all ease 0.2s;
 }
 
 .menu {
@@ -159,19 +170,28 @@ class DashboardIndex extends ServiceInjectorMixin(PolymerElement) {
 }
 
 
-.flex div a {
+.icon {
   display: block;
   background: #015b63;
-  width: 80%;
-  padding-top: 80%;
   border-radius: 17px;
-  margin: auto auto;
+  text-align: center;
+  margin: auto 10%;
+  line-height: 1;
+  padding: 15%;
+}
+
+.icon:hover {
+  transition: all ease 0.2s;
+  padding: 10%;
+}
+
+.icon img {
+  max-width: 100%;
 }
 
 .flex div p {
   font-size: 18px;
-  max-width: 80%;
-  margin: 5px auto;
+  margin: 5px 10%;
   line-height: 1.3em;
 }
 
@@ -276,33 +296,40 @@ class DashboardIndex extends ServiceInjectorMixin(PolymerElement) {
   color: black;
   text-align: center;
   margin: 60px auto;
-  max-width: 800px;
+  max-width: 960px;
   font-size: 22px;
+  display: flex;
 }
 
 .review{
-
-}
-
-.quote:hover .review{
-display:none;
-}
-
-  .review2{
     display: none;
     margin-right: -800px;
     transition: all ease 0.2s;
 }
 
-.quote:hover .review2 {
+.review.active{
   display:block;
-  transition: all ease 0.2s;
-margin-right:0;
+  align-self:center;
+    margin:0 50px;
 }
 
   .navigation{
 
   }
+
+  .navigation .dot{
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background: #015b63;
+    margin: 0 10px;
+    display: inline-block;
+}
+
+.navigation .dot:hover, .navigation .dot.active{
+  cursor:pointer;
+  background: white;
+}
   
   .row.team {
     width: 100%;
@@ -311,6 +338,92 @@ margin-right:0;
     margin: 50px auto 20px;
 }
 
+
+.left, .right {
+  font-size: 130px;
+  font-family: auto;
+  color:#015b63;
+}
+
+.left:hover, .right:hover {
+  color:white;
+  cursor:pointer;
+}
+
+#how{
+  display:block;
+}
+
+#how .text-block{
+  max-width:1200px;
+}
+
+#how .text-block .flex .step{
+  width: 30%;
+}
+
+#how .text-block .flex .step div {
+  display: block;
+  background: url(http://127.0.0.1:8081/images/sfondo.jpg);
+  border-radius: 15px;
+  position: relative;
+  background-size: cover;
+  padding-bottom: 50px;
+  width: 100%;
+  min-height: 500px;
+}
+
+#how .text-block .flex .step:first-child div {
+  background-position:top left;
+}
+
+#how .text-block .flex .step:nth-child(2) div {
+  background-position:center center;
+}
+
+#how .text-block .flex .step:nth-child(3) div {
+  background-position:bottom right;
+}
+
+#how .text-block .flex .step div:after {
+  content: "";
+  display: inline-block;
+  font-size: 1.6em;
+  height: 80px;
+  width: 80px;
+  background: white;
+  border-radius: 50%;
+  border: 5px solid #f0b906;
+  position: absolute;
+  bottom: -50px;
+  left: 50%;
+  line-height: 80px;
+  z-index: 100;
+  transform: translateX(-50%);
+}
+
+#how .text-block .flex .step:first-child div:after {
+  content:"1";
+  }
+
+#how .text-block .flex .step:nth-child(2) div:after {
+    content:"2";
+  }
+    
+#how .text-block .flex .step:nth-child(3) div:after {
+    content:"3";
+  }
+  
+
+#how .text-block .flex .step img{
+  width: 100%;
+  filter: brightness(0.5);
+}
+
+#how .text-block .flex .step p{
+  margin-top: 70px;
+  font-size: 22px;
+}
 
   .padding-34 {
             padding: 34px;
@@ -456,7 +569,7 @@ margin-right:0;
           <div class="container box">
             <div class="intro-description center start">
                 <h2 class="margin-0">Hai mai pensato a un menu digitale per il tuo ristorante?</h2>
-                <a class="button" href="#how">Scopri di piu</a>
+                <a class="button yellow" href="#how">Scopri di piu</a>
             </div>
             </div>
             </div>
@@ -469,67 +582,47 @@ margin-right:0;
                 <p class="">Di addio alle continue ristampe dei menu cartacei, all'usura e alla loro poco igienicità.<br/>Da oggi c'è Dsign Menu, il primo <b>menu interamente digitale</b>, l'ieale per i tuoi clienti!<br/>FACILE, VELOCE E PERSONALIZZABILE.</p>
                 <div class="flex">
                 <div class="">
-                <a class="icon" href="#"></a><p>Facile</p>
+                <a class="icon"><img src="http://127.0.0.1:8081/images/ico1.png" /></a><p>Facile</p>
                 </div>
                 <div class="">
-                <a class="icon" href="#"></a><p>Personalizzabile</p>
+                <a class="icon"><img src="http://127.0.0.1:8081/images/ico2.png" /></a><p>Personalizzabile</p>
                 </div>
                 <div class="">
-                <a class="icon" href="#"></a><p>Sempre aggiornato</p>
+                <a class="icon"><img src="http://127.0.0.1:8081/images/ico3.png" /></a><p>Sempre aggiornato</p>
                 </div>
                 <div class="">
-                <a class="icon" href="#"></a><p>Garantito dai clienti</p>
+                <a class="icon"><img src="http://127.0.0.1:8081/images/ico6.png" /></a><p>Garantito dai clienti</p>
                 </div>
                 <div class="">
-                <a class="icon" href="#"></a><p>Veloce</p>
+                <a class="icon"><img src="http://127.0.0.1:8081/images/ico4.png" /></a><p>Veloce</p>
                 </div>
                 <div class="">
-                <a class="icon" href="#"></a><p>Rispettoso dell'ambiente</p>
+                <a class="icon"><img src="http://127.0.0.1:8081/images/ico5.png" /></a><p>Rispettoso dell'ambiente</p>
                 </div>
             </div>
             </div>
             </div>
           </div>     
       <div class="section column white">
-      <div>     
         <div id="how" class="row center">
             <h2>Come Funziona?</h2>
-        </div>
-        <div class="row strengths padding-34 space-between">
-            <paper-card strengths image="http://placehold.it/300x150/015b63/ffffff">
-                <div class="card-content">
-                    <h3>Gratuito</h3>
-                      <div class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
+            <div class="text-block">
+              <p class="">Niente di più semplice, registrati gratuitamente, crea il tuo menu, stampa il QR Code per i tuoi tavoli e guarda i tuoi clienti utilizzare il tuo menu!</p>
+              <div class="flex">
+                <div class="step">
+                  <div class=""></div><p>Registrati</p>
                 </div>
-            </paper-card>
-            <paper-card strengths image="http://placehold.it/300x150/015b63/ffffff">
-                <div class="card-content">
-                    <h3>Personalizzabile</h3>
-                    <div class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
+                <div class="step">
+                  <div class=""></div><p>Crea il tuo menu</p>
                 </div>
-            </paper-card>
-            <paper-card strengths image="http://placehold.it/300x150/015b63/ffffff">
-                <div class="card-content">
-                    <h3>Personalizzabile</h3>
-                    <div class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
+                <div class="step">
+                  <div class=""></div><p>Stampa il tuo QR Code</p>
                 </div>
-            </paper-card>
-            <paper-card strengths image="http://placehold.it/300x150/015b63/ffffff">
-                <div class="card-content">
-                    <h3>Utile</h3>
-                      <div class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
-                </div>
-            </paper-card>
-            <paper-card strengths image="http://placehold.it/300x150/015b63/ffffff">
-                <div class="card-content">
-                    <h3>Facile</h3>
-                      <div class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
-                </div>
-            </paper-card>
-        </div>            
-        <a class="button" href="#how">Provalo subito</a>
+              </div>
+            </div>         
+            <a class="button" href="#how">Provalo subito</a>
         </div>     
-        </div>
+      </div>
       <div class="section column blue">
          <div class="row center">
             <h2>Quanto mi costa?</h2>
@@ -539,7 +632,7 @@ margin-right:0;
               <ul>
               <li>Piatti illimitati</li>
               <li>2 template predefiniti</li>
-              <li>Assistenza online</li>
+              <li>Nessuna APP da scaricare</li>
               <li>Generazione e stampa QR code</li>
               <hr/>
               <li><span>Gratis</span></li>
@@ -551,11 +644,13 @@ margin-right:0;
           <div class="container box">
             <div class="people center start">
                 <h2 class="margin-0">Dicono di noi</h2>
-                <div class="quote">
-                  <div class="review">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
-                  <div class="review2">Lorem Ipsum doloret sit amet, consectetuer adipiscing elit, sed diam n. Duis autem vel eum iriure dolor in hendrerit in vuluputate velit esse molestie consequat</div>
-                </div>
-                <div class="navigation"> x x x </div>
+                <div class="quote"><div class="left"><</div>
+                  <div class="review active">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
+                  <div class="review">Lorem Ipsum doloret sit amet, consectetuer adipiscing elit, sed diam n. Duis autem vel eum iriure dolor in hendrerit in vuluputate velit esse molestie consequat</div>
+                  <div class="review">Lorem Ipsum doloret sit amet, is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</div>
+                  <div class="right">></div>
+                  </div>
+                <div class="navigation"><div class="dot active"></div><div class="dot"></div><div class="dot"></div></div>
                 </div>
             </div>
           </div>      
@@ -576,7 +671,7 @@ margin-right:0;
                 </div>
                 <div  class="user_team">
                     <div class="user_photo"></div>
-                    <div class="role">Web Designer</div>
+                    <div class="role">Designer</div>
                     <div class="name">Paolo Sartorio</div>
                 </div>
            </div>
