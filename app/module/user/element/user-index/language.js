710cc3d76acd3af8e521f8d04635e46658bc0025
visa4 <antonino.visalli@gmail.com>
2023-10-10 16:55:05 +0200

    Fix user papar element

--- app/module/user/element/user-index/language.js
+++ app/module/user/element/user-index/language.js
@@ -4,11 +4,13 @@ export const lang = {
     "insert-user" : "Inserisci risorsa",
     "update-user" : "Modifica risorsa",
     "back" : "Indietro",
+    "name": "Nome"
   },
   "en": {
     "list-user": "List user",
     "insert-user" : "Insert monitor",
     "update-user" : "Update monitor",
     "back" : "Back",
+    "name": "Name"
   }
 };
