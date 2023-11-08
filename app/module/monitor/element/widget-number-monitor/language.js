71eb80c20ea1fe8ccc2b61d145bf4cd74d926643
visa4 <antonino.visalli@gmail.com>
2023-11-08 17:15:03 +0100

    Fix resource

---
+++ app/module/monitor/element/widget-number-monitor/language.js
@@ -0,0 +1,10 @@
+export const lang = {
+  "it": {
+    "title": "Impianti",
+    "sub-title": "Totale dispositivi registrati",
+  },
+  "en": {
+    "title": "Plant",
+    "sub-title": "Totale dispositivi registrati",
+  }
+};
---
+++ app/module/resource/element/widget-number-resource/language.js
@@ -0,0 +1,10 @@
+export const lang = {
+  "it": {
+    "title": "Risorse",
+    "sub-title": "Totale dispositivi registrati",
+  },
+  "en": {
+    "title": "Resources",
+    "sub-title": "Totale dispositivi registrati",
+  }
+};
